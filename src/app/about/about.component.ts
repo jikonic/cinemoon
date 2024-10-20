f02cc0e4c26efaf8c5cc5855c9ea482db106d244
jikonic <jikonic2002@gmail.com>
2024-10-20 22:33:48 +0200

    about

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { MatList, MatListItem } from '@angular/material/list';
 
 @Component({
   selector: 'app-about',
   standalone: true,
-  imports: [],
+  imports: [MatList,MatListItem],
   templateUrl: './about.component.html',
   styleUrl: './about.component.css'
 })
